68b21d3bab33c32434107b291f90146573ffba53
Anne Zou <anne.zou@vanderbilt.edu>
2017-07-10 17:15:15 -0500

    Added CursorAdapter

---
+++ app/src/main/java/edu/vanderbilt/isis/trip_planner_android_client/model/database/SearchHistoryCursorAdapter.java
@@ -0,0 +1,28 @@
+package edu.vanderbilt.isis.trip_planner_android_client.model.database;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
+/**
+ * Created by Anne on 7/10/2017.
+ */
+
+public class SearchHistoryCursorAdapter extends CursorAdapter {
+
+    public SearchHistoryCursorAdapter(Context context, Cursor cursor) {
+        super(context, cursor, 0);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
+}
